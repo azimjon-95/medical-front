9b21a2a5958f79aa9f07217deda26d24a8d1ac75
abdulhayevbahrom <bahromjonabdulhayev2002@gmail.com>
2023-11-28 13:42:46 +0500

    changet some files

--- src/pages/admin/swiper/Carousel.jsx
+++ src/pages/admin/swiper/Carousel.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Carousel.css'
 import { FaUserDoctor } from "react-icons/fa6";
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
+import { NumberFormat } from '../../../hook/NumberFormat'
 
 
 
-function DoctorsSlite({ doctor }) {
+function DoctorsSlite({ doctor, users }) {
+    const [qty, setQty] = useState([])
+    console.log(qty);
 
     let data = doctor.filter((i) => i.docORrecep === "doctor");
     let width = window.innerWidth
+
+    let time = new Date()
+    let day = time.getDate() + "." + (time.getMonth() + 1) + "." + time.getFullYear();
     return (
         <div className='carousel'>
             <Swiper
@@ -26,15 +32,26 @@ function DoctorsSlite({ doctor }) {
 
                     data?.map((value, inx) => {
                         return (
-                            <SwiperSlide>
+                            <SwiperSlide key={inx} >
                                 <div className='card'>
                                     <div className='card-inner'>
                                         <FaUserDoctor className='card_icon' />
-                                        <span className='doctorname'>{value.firstName} <br /> {value.lastName}</span>
+                                        <span className='doctorname'>{value.firstName}.{value.lastName[0]}</span>
                                         <b>{value.specialization}</b>
                                     </div>
                                     <div className="allInfoTotal">
-
+                                        <b>
+                                            {
+                                                NumberFormat(
+                                                    users?.filter(user => user.choseDoctor === value.specialization
+                                                        && user.view === true
+                                                        && user.day === day)
+                                                        // ?.map(i => qty.push(i.paySumm))
+                                                        .map(i => i.paySumm)
+                                                        .reduce((a, b) => a + b, 0)
+                                                )
+                                            }
+                                        </b>
                                     </div>
                                 </div>
                             </SwiperSlide>
